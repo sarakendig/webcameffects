9471c5c8bd6d5b547f4f1d5533e7326fab667191
sarakendig <sarakendig@gmail.com>
2020-12-01 00:10:24 -0500

    webcam loads to page

---
+++ app.js
@@ -0,0 +1,14 @@
+navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+    })
+    .then(stream => {
+        const video = document.getElementById('webcam');
+        video.srcObject = stream;
+        video.onloadedmetadata = (e) => {
+            video.play();
+        };
+    })
+    .catch(error => {
+        alert('Please enable camera permissions')
+    })
